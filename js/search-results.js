5b5f52e10536a5cff667d85d5a7cfedaeae835f4
TomyWes <tomyweskamp@github.com>
2023-06-05 22:40:02 -0300

    Search results funciona

--- js/search-results.js
+++ js/search-results.js
@@ -1,7 +1,59 @@
 const loader = document.querySelector('.loader');
+const searchResultsSection = document.querySelector('#search-results');
+const noResults = document.querySelector('#no-results');
 
 loader.classList.remove('hidden');
 
 setTimeout(() => {
   loader.classList.add('hidden');
 }, 3000);
+
+const queryString = window.location.search;
+const searchParams = new URLSearchParams(queryString);
+const searchTerm = searchParams.get('term');
+
+if (!searchTerm) {
+  noResults.style.display = 'block';
+} else {
+  const searchUrl = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${encodeURIComponent(searchTerm)}`;
+
+  fetch(searchUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+
+      loader.classList.add('hidden');
+
+      if (data.data.length === 0) {
+        noResults.style.display = 'block';
+        return;
+      }
+
+      let searchResultsHTML = '';
+
+      searchResultsHTML += `<h1 class='raleway'>Resultados de búsqueda para: ${searchTerm}</h1>`;
+      searchResultsHTML += '<ul class="search-results-ul opensans">';
+
+      for (let i = 0; i < data.data.length; i++) {
+        const result = data.data[i];
+        let detailUrl = '';
+
+        if (result.type === 'track') {
+          detailUrl = `detail-song.html?id=${result.id}`;
+        } else if (result.type === 'artist') {
+          detailUrl = `detail-artist.html?id=${result.id}`;
+        } else if (result.type === 'album') {
+          detailUrl = `detail-album.html?id=${result.id}`;
+        }
+
+        searchResultsHTML += `<li><a href="${detailUrl}">${result.title}</a></li>`;
+      }
+
+      searchResultsSection.innerHTML = searchResultsHTML;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
