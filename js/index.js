b1c0facacd62bafb364246a67e6ba9fcf8dad75e
joacobaiardi <jbaiardi@udesa.edu.ar>
2023-06-09 01:32:34 -0300

    estilos varios

--- js/index.js
+++ js/index.js
@@ -128,7 +128,7 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks
 
     let songsHTML = '';
 
-    for (let i = 0; i < songs.length; i++) {
+    for (let i = 0; i < 8; i++) {
       const song = songs[i];
 
       songsHTML += `
@@ -160,7 +160,7 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/albums
 
     let albumsHTML = '';
 
-    for (let i = 0; i < albums.length; i++) {
+    for (let i = 0; i < 8; i++) {
       const album = albums[i];
 
       albumsHTML += `
@@ -191,7 +191,7 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artist
 
     let artistsHTML = '';
 
-    for (let i = 0; i < artists.length; i++) {
+    for (let i = 0; i < 8; i++) {
       const artist = artists[i];
 
       artistsHTML += `
